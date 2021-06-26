e1526657691ca57547626597ccdbe5cd1df8889e
Jadson Santos <jadson44.santos@gmail.com>
2021-06-26 13:45:16 -0300

    update

--- api/mod.ts
+++ api/mod.ts
@@ -1,10 +1,7 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
-import { config } from "https://deno.land/x/dotenv/mod.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import router from "./routes/routes.ts";
 
-const HOST = config().HOST ?? "127.0.0.1";
-const PORT = config().PORT ?? "8085";
 
 const app = new Application();
 
@@ -16,5 +13,5 @@ app.use(
 );
 app.use(router.routes());
 
-console.log(`Deno is running: ${HOST}:${PORT}`);
-await app.listen({ port: Number(PORT)});
+
+addEventListener("fetch", app.fetchEventHandler());
