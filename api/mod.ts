24b31fbbc762d6e4ba79fb752856d3ee6673cd74
Jadson Santos <42282908+jadson179@users.noreply.github.com>
2021-06-27 11:54:17 -0300

    Update mod.ts

--- api/mod.ts
+++ api/mod.ts
@@ -1,16 +1,8 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
-import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import router from "./routes/routes.ts";
 
 
 const app = new Application();
-
-app.use(
-  oakCors({
-    origin: "*",
-    optionsSuccessStatus: 200,
-  }),
-);
 app.use(router.routes());
 
 
