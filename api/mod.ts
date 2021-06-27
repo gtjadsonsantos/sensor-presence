21c215ad992e37baa92fb9d1f7009c1c549b47eb
Jadson Santos <42282908+jadson179@users.noreply.github.com>
2021-06-27 14:00:22 -0300

    Update mod.ts

--- api/mod.ts
+++ api/mod.ts
@@ -1,9 +1,9 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
-import router from "./routes/routes.ts";
+//import router from "./routes/routes.ts";
 
 
 const app = new Application();
-app.use(router.routes());
+//app.use(router.routes());
 
 
 addEventListener("fetch", app.fetchEventHandler());
